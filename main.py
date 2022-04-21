8ac922487fb74cc2c91e61b89e3ed34c9595d8d2
ewe08 <wileere@yandex.ru>
2022-04-21 20:53:04 +0700

    sdsdasdasdas

--- main.py
+++ main.py
@@ -218,4 +218,4 @@ def object_comments(id):
 if __name__ == '__main__':
     db_sess = db_session.global_init(f"db/Culture.db")
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='127.0.0.1', port=port, debug=True)
+    app.run(host='0.0.0.0', port=port, debug=True)
